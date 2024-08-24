a52e50dd32b207d87b6e59adfa8933eb42df4011
yousefn <jon772002@gmail.com>
2024-08-24 07:53:09 +0300

    course table buttons alignment

--- src/components/shared/tables/CourseRow.jsx
+++ src/components/shared/tables/CourseRow.jsx
@@ -1,5 +1,5 @@
 "use client";
-import "./courseRow.css";
+import "./courseRow.dev.css";
 import Link from "next/link";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
