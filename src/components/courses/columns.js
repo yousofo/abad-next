f3f315582912fe714f3706fa0b80d798f5446178
yousefn <jon772002@gmail.com>
2024-09-25 14:23:49 +0300

    btns glitch fix

--- src/components/courses/columns.js
+++ src/components/courses/columns.js
@@ -79,7 +79,7 @@ export function useColumns() {
       accessor: "formattedTimeStart",
       Cell: ({ row }) =>
         !row.original.month ? (
-          <div className="flex items-center gap-1">
+          <div className="flex items-center gap-1 course-time">
             <span className="sm:hidden">التوقيت</span>
             <span className="sm:hidden">:</span>
             <span>
@@ -165,15 +165,15 @@ export function useColumns() {
                   >
                     <path
                       d="M10.8866 7.99995C10.8866 9.31995 9.81995 10.3866 8.49995 10.3866C7.17995 10.3866 6.11328 9.31995 6.11328 7.99995C6.11328 6.67995 7.17995 5.61328 8.49995 5.61328C9.81995 5.61328 10.8866 6.67995 10.8866 7.99995Z"
-                      fill="#3F3E43"
-                      stroke="#3F3E43"
+                      fill="none"
+                      stroke="currentColor"
                       strokeWidth="1.5"
                       strokeLinecap="round"
                       strokeLinejoin="round"
                     />
                     <path
                       d="M8.4999 13.5131C10.8532 13.5131 13.0466 12.1264 14.5732 9.7264C15.1732 8.7864 15.1732 7.2064 14.5732 6.2664C13.0466 3.8664 10.8532 2.47974 8.4999 2.47974C6.14656 2.47974 3.95323 3.8664 2.42656 6.2664C1.82656 7.2064 1.82656 8.7864 2.42656 9.7264C3.95323 12.1264 6.14656 13.5131 8.4999 13.5131Z"
-                      stroke="#3F3E43"
+                      stroke="currentColor"
                       strokeWidth="1.5"
                       strokeLinecap="round"
                       strokeLinejoin="round"
